20b364049cccff4c5f18afedea44c2d02187b1eb
Shreya Mishra <shreyamishrapbh2006@gmail.com>
2026-02-14 22:13:32 +0530

    Add files via upload

---
+++ py9.py
@@ -0,0 +1,16 @@
+def twoSum(nums, target):
+    num_map = {}
+    
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        
+        if complement in num_map:
+            return [num_map[complement], i]
+        
+        num_map[nums[i]] = i
+
+
+# Example usage
+nums = [2, 7, 11, 15]
+target = 9
+print(twoSum(nums, target))
